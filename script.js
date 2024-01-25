14eb62281165fdba633da6889484fd9e0dfb3491
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2024-01-25 15:44:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+	const form=document.querySelector("form"),
+button=form.querySelector("#btn"),
+output=document.getElementById("output");
+	
+button.addEventListener('click',(e)=>{
+  //I'll have this even objec..
+  let text=document.getElementById("text").value;
+  let delay=document.getElementById("delay").value;
+  synchFun(text,parseInt(delay));
+
+})
+
+async function synchFun(text,delay) {
+ await displayText(text,delay);
+ 
+}
+
+function displayText(text,delay) {
+  setTimeout(() => {
+    output.innerText=text;
+  }, delay);
+}
