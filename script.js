3abb6fa2db9e4266481b3a0b5ae9fb62a53d5738
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2024-01-29 14:29:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ let form = document.querySelector("form");
 let outputDiv = document.getElementById("output");
 
 form.addEventListener('submit', (event) => {
+  event.preventDefault();  // prevent page reload
   let text = event.target.text.value;
   let delay = event.target.delay.value;
-  //we have the Mannat
   displayText(text, delay);
 })
 
 async function displayText(text, delay) {
   try {
     let promise = await getPromise(text,delay);
-   outputDiv.innerText=promise;
+    outputDiv.innerText=promise;
   }
   catch (err) {
     console.log(err);
